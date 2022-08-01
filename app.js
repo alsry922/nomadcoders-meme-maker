1e26ed8a6ed0e42117b176938213bba47942f4be
alsry922 <alsry922@gmail.com>
2022-08-01 19:11:11 +0900

    canvas path

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const $canvas = document.querySelector("canvas");
 const ctx = $canvas.getContext("2d");
+$canvas.width = 800;
+$canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200);
+ctx.rect(0, 0, 50, 50);
+ctx.rect(50, 50, 50, 50);
+ctx.rect(100, 100, 50, 50);
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(150, 150, 50, 50);
+ctx.rect(200, 200, 50, 50);
+ctx.fillStyle = "grey";
+ctx.fill();
